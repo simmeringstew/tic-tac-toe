88899095c570bdfcb0c8b3752fbb848f48bfa5d6
simmeringstew <dev.95a2e@simplelogin.com>
2022-10-21 12:33:13 -0400

    Added ability for player and computer to make moves

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ async function initializeGame() {
         tile.addEventListener("click", gameLoop);
     });
     const playerPosition = getRandom(2);
-    let computerPosition = 0;
+    let computerPosition = undefined;
     if (playerPosition === 0) {
         computerPosition = 1;
     }
@@ -89,31 +89,49 @@ async function initializeGame() {
     await sleep(2000);
     gameInfo.classList.add("hidden");
     if (computerPosition === 0) {
-        computerFirstTurn();
+        computerMove();
     }
 }
 
 // handles the basic gameloop of the game
 
 function gameLoop() {
-    console.log(window.gameboard.board);
+    let victoryCheck = undefined;
+    const playerTile = this.getAttribute("data-key");
+    if (isNaN(window.gameboard.board[playerTile]) === true) {
+        return;
+    }
+    this.classList.add(window.player.color);
+    this.textContent = window.player.letter;
+    window.gameboard.board[playerTile] = window.player.letter;
+    victoryCheck = checkVictory();
 }
 
+
+// function that allows the computer to make its move
+
+function computerMove() {
+
+    let selection = undefined;
+    while (true) {
+        selection = window.computer.computerTurn;
+        if (isNaN(window.gameboard.board[selection]) === false) {
+            break;
+        }
+    }
+    console.log(selection);
+    const selectedTile = document.querySelector(`div[data-key="${selection}"]`);
+    selectedTile.classList.add(window.computer.color);
+    selectedTile.textContent = window.computer.letter;
+    window.gameboard.board[selection] = window.computer.letter;    
+}
+
+// function that checks for victory
 
 // function handles getting random numbers for assigning who goes first and the computers moves
 
 function getRandom(max) {
     return Math.floor(Math.random() * max);
-}
-
-// function that allows the computer to make its first move
-
-function computerFirstTurn() {
-    const selection = window.computer.computerTurn;
-    const selectedTile = document.querySelector(`div[data-key="${selection}"]`);
-    selectedTile.textContent = window.computer.letter;
-    selectedTile.classList.add(window.computer.color);
-    window.gameboard.board[selection] = window.computer.letter;    
 }
 
 // function to have the program sleep
